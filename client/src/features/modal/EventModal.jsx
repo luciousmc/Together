aaf3e922f0c53dddd7dad34ef38acebd3fddebc4
Marlon Clay <luciousmc@icloud.com>
2023-03-18 10:40:59 -0700

    date section

--- client/src/features/modal/EventModal.jsx
+++ client/src/features/modal/EventModal.jsx
@@ -10,6 +10,7 @@ import { useModalContext } from "contexts/ModalContext";
 import dataService from "../../services/dataService";
 import { useAuthContext } from "contexts/AuthContext";
 import { AiOutlineClose } from "react-icons/ai";
+import { CiGlobe } from "react-icons/ci";
 
 function EventModal(props) {
   return (
@@ -20,6 +21,20 @@ function EventModal(props) {
           <AiOutlineClose />
         </button>
       </header>
+      <section className="flex items-center gap-4 p-3">
+        <div className="bg-[#E1E1E1] w-[15%] p-2 text-center rounded-md font-semibold">
+          <h2 className="text-[#FF8435]">Feb</h2>
+          <h2>2</h2>
+        </div>
+        <div>
+          <h2 className="font-semibold">Thursday</h2>
+          <h2 className="font-semibold">3:30 PM - 4:30 PM</h2>
+          <div className="flex items-center gap-1 text-sm">
+            <CiGlobe className="text-[#3EA6D7] " />
+            <span className="text-[#989898]">America/Chicago</span>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
