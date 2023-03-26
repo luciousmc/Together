344297fb747b6718ed46bc79498b8191e4a50695
Marlon Clay <luciousmc@icloud.com>
2023-03-26 07:47:33 -0700

    feat: adds event data to modal

--- client/src/features/modal/EventModal.jsx
+++ client/src/features/modal/EventModal.jsx
@@ -10,26 +10,40 @@ import { FaRegCalendarCheck } from "react-icons/fa";
 import { GrLocation } from "react-icons/gr";
 import { FcExpand } from "react-icons/fc";
 
-function EventModal(props) {
+function EventModal() {
+  const {
+    activeEvent: { title, startAt, endAt, location },
+    handleClose,
+  } = useModalContext();
+
+  const month = format(parseISO(startAt), "MMM");
+  const date = format(parseISO(startAt), "d");
+  const day = format(parseISO(startAt), "eeee");
+  const timeRange = `
+    ${formatToLocalTime(startAt)} -
+    ${formatToLocalTime(endAt)}
+   `;
+  const timeZone = "America/Chicago";
+
   return (
     <div className="bg-white font-inconsolata rounded-md">
       <header className="flex items-center justify-between p-3 border-b-4">
-        <h1 className="font-semibold text-xl">Banki Session</h1>
+        <h1>{title}</h1>
         <button className="border-2 p-1 border-black rounded-md">
-          <AiOutlineClose />
+          <AiOutlineClose onClick={handleClose} />
         </button>
       </header>
       <section className="flex items-center gap-4 p-4">
         <div className="bg-[#E1E1E1] w-[15%] p-2 text-center rounded-md font-semibold">
-          <h2 className="text-[#FF8435]">Feb</h2>
-          <span>2</span>
+          <h2 className="text-[#FF8435]">{month}</h2>
+          <span>{date}</span>
         </div>
         <div>
-          <h2 className="font-semibold">Thursday</h2>
-          <span className="font-semibold">3:30 PM - 4:30 PM</span>
+          <h2 className="font-semibold">{day}</h2>
+          <span className="font-semibold">{timeRange}</span>
           <div className="flex items-center gap-1 text-sm">
             <CiGlobe className="text-[#3EA6D7] " />
-            <span className="text-[#989898]">America/Chicago</span>
+            <span className="text-[#989898]">{timeZone}</span>
           </div>
         </div>
         <div>
@@ -40,11 +54,11 @@ function EventModal(props) {
       </section>
       <div className="p-4">
         <h1 className="font-semibold mb-3 text-xl">Description</h1>
-        <h2 className="mt-3 border-2 p-1.5 rounded-md">
+        <p className="mt-3 border-2 p-1.5 rounded-md">
           Daily Banki streams hosted on Discord to practice interview questions.
           You do not need to be confident in your responses to participate - all
           are weclome!
-        </h2>
+        </p>
         <div className="flex flex-col items-center justify-center font-semibold">
           <button>
             <FcExpand />
@@ -55,18 +69,8 @@ function EventModal(props) {
         <h1 className="font-semibold text-xl">Location</h1>
         <div className="flex items-center gap-1 text-base">
           <GrLocation className="text-[#3EA6D7] " />
-          <span>Voice-01</span>
+          <span>{location}</span>
         </div>
-      </div>
-      <div className="flex flex-col items-center justify-center font-semibold p-6">
-        <div className="inline-flex gap-2">
-          <h3>Click</h3>
-          <h3 className="text-[#FF8435]">Login</h3>
-          <h3>to join the event</h3>
-        </div>
-        <button className="p-2.5 bg-[#E1E1E1] border-2 rounded-3xl m-0 text-[#FF8435] text-xl drop-shadow-[rgdb(0,0,0,0.25)]">
-          <h1>Login with Discord</h1>
-        </button>
       </div>
     </div>
   );
