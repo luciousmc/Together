0af9a04a9822ce5d5d95aee8b45b0ea24213a5fb
Marlon Clay <luciousmc@icloud.com>
2023-03-29 11:10:50 -0700

    minor style adjustments and made description dynamic

--- client/src/features/modal/EventModal.jsx
+++ client/src/features/modal/EventModal.jsx
@@ -6,14 +6,13 @@ import { useAuthContext } from "contexts/AuthContext";
 import { AiOutlineClose } from "react-icons/ai";
 import { CiGlobe } from "react-icons/ci";
 import { FaRegCalendarCheck } from "react-icons/fa";
-import { GrLocation } from "react-icons/gr";
-import { FcExpand } from "react-icons/fc";
+import { IoLocationSharp } from "react-icons/io5";
 import { RxAvatar } from "react-icons/rx";
 
 function EventModal() {
   const { isAuthenticated } = useAuthContext();
   const {
-    activeEvent: { title, startAt, endAt, location, user: author },
+    activeEvent: { title, description, startAt, endAt, location, user: author },
     handleClose,
   } = useModalContext();
 
@@ -29,7 +28,7 @@ function EventModal() {
   return (
     <div className="bg-white font-inconsolata rounded-md">
       <header className="flex items-center justify-between p-3 border-b-4">
-        <h1>{title}</h1>
+        <h1 className="font-extrabold uppercase">{title}</h1>
         <button
           onClick={handleClose}
           className="border-2 p-1 border-black rounded-md"
@@ -38,12 +37,12 @@ function EventModal() {
         </button>
       </header>
       <section className="flex items-center gap-4 py-4 px-7">
-        <div className="bg-[#E1E1E1] w-[15%] p-2 text-center rounded-md font-semibold">
+        <div className="bg-[#E1E1E1] w-[15%] py-2 text-center rounded-md font-semibold">
           <h2 className="text-[#FF8435]">{month}</h2>
           <span>{date}</span>
         </div>
         <div className="flex-grow">
-          <h2 className="font-semibold">{day}</h2>
+          <h2 className="font-semibold text-lg">{day}</h2>
           <span className="font-semibold">{timeRange}</span>
           <div className="flex items-center gap-1 text-sm">
             <CiGlobe className="text-[#3EA6D7] " />
@@ -56,22 +55,15 @@ function EventModal() {
           </button>
         </div>
       </section>
-      <div className="p-4 flex flex-col items-center">
-        <h2 className="font-semibold mb-3 text-xl self-start">Description</h2>
-        <p className="mt-3 border-2 p-1.5 rounded-md">
-          Daily Banki streams hosted on Discord to practice interview questions.
-          You do not need to be confident in your responses to participate - all
-          are weclome!
-        </p>
-        <button>
-          <FcExpand />
-        </button>
+      <div className="p-4">
+        <h2 className="font-semibold mb-3 text-xl">Description</h2>
+        <p className="mt-3 border-2 px-1.5 py-3 rounded-md">{description}</p>
       </div>
       <div className="py-4 px-14 flex justify-between">
         <div className="flex flex-col items-center">
           <h2 className="font-semibold text-xl">Location</h2>
           <div className="text-base flex flex-grow items-center justify-center gap-2">
-            <GrLocation className="text-[#3EA6D7]" />
+            <IoLocationSharp className="text-[#3EA6D7]" />
             <span>{location}</span>
           </div>
         </div>
@@ -85,11 +77,15 @@ function EventModal() {
                 {author.avatar ? (
                   <img
                     className="w-full h-full object-contain rounded-full"
+                    // should be able to use avatarURL
+                    // need to figure out how to get this virtual field from backend
+                    // currently passing "avatar" and "discordId" fields from db
+                    // src={author.avatarURL}
                     src={`https://cdn.discordapp.com/avatars/${author.discordId}/${author.avatar}.webp`}
                     alt="Avatar"
                   />
                 ) : (
-                  <RxAvatar className="w-5" />
+                  <RxAvatar className="w-10 h-10" />
                 )}
               </div>
               <div>
