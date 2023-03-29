6e96c0ca5fe68fadbe502ce4fd2a7a2ae5ce0f1d
Marlon Clay <luciousmc@icloud.com>
2023-03-29 13:58:06 -0700

    grab user object from auth

--- client/src/features/modal/EventModal.jsx
+++ client/src/features/modal/EventModal.jsx
@@ -10,9 +10,22 @@ import { IoLocationSharp } from "react-icons/io5";
 import { RxAvatar } from "react-icons/rx";
 
 function EventModal() {
-  const { isAuthenticated } = useAuthContext();
   const {
-    activeEvent: { title, description, startAt, endAt, location, user: author },
+    isAuthenticated,
+    user: { _id: userId },
+  } = useAuthContext();
+
+  const {
+    activeEvent: {
+      _id,
+      title,
+      description,
+      startAt,
+      endAt,
+      groupId,
+      location,
+      user: author,
+    },
     handleClose,
   } = useModalContext();
 
@@ -24,6 +37,10 @@ function EventModal() {
     ${formatToLocalTime(endAt)}
    `;
   const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+  const handleJoinclick = () => {
+    console.log(_id, userId);
+  };
 
   return (
     <div className="bg-white font-inconsolata rounded-md">
@@ -99,8 +116,11 @@ function EventModal() {
           </div>
         )}
       </div>
-      {/*<div className="flex flex-col items-center justify-center font-semibold p-6">
-        <button className="p-2.5 bg-[#FF8435]  text-white border-2 rounded-3xl m-0 text-xl w-40 border-b-4">
+      <div className="flex flex-col items-center justify-center font-semibold p-6">
+        <button
+          onClick={handleJoinclick}
+          className="p-2.5 bg-[#FF8435] border-2 text-white rounded-3xl m-0 text-xl w-40 border-b-4"
+        >
           Join
         </button>
         <div className="mt-5 p-4 border-t-4">
@@ -121,7 +141,8 @@ function EventModal() {
             </button>
           </div>
         </div>
-      </div>*/}
+      </div>
+      */}
       <div className="flex flex-col items-center justify-center font-semibold ">
         <button className="p-2.5 bg-[#165E7C]/75 text-white border-2 rounded-3xl m-0 text-xl w-40 border-b-4">
           Withdraw
